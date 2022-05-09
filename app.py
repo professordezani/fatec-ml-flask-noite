fccca05e596d37a4b0ee3249905e414c4a38e63f
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2022-05-09 23:08:43 +0000

    adiciona entrada/saida

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ import cloudpickle
 with open('model.pkl', 'rb') as file_in:
   model_loaded = cloudpickle.load(file_in)
 
-print(model_loaded.predict(['péssimo']))
+frase = input('Digite uma frase: ')
+
+if model_loaded.predict([frase])[0] == 0:
+    print('Comentário negativo')
+else:
+    print('Comentário positivo')
