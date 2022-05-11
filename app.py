48665a2e47703a0ac15a0907806bd38fcdec55d4
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2022-05-11 01:09:25 +0000

    versao final sem bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import cloudpickle
+from flask import Flask, render_template, request
 
 with open('model.pkl', 'rb') as file_in:
   model = cloudpickle.load(file_in)
 
-print(model.predict(['bom']))
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html', nome='Fulano')
+
+@app.route('/predicao', methods=['POST'])
+def predicao():
+  comentario = request.form['comentario']
+  predicao = model.predict([comentario])
+  return render_template('resposta.html', predicao=predicao[0])
+
+app.run(debug=True)
+
+# pip install -r requirements.txt (instala as bibliotecas)
+# python app.py (para executar)
