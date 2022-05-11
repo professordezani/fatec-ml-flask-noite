2d53e712b573c3436a5b8225f571a6b40e18b59c
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2022-05-11 00:00:59 +0000

    Aplicação Web

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
+from flask import Flask, render_template, request
 import cloudpickle
 
 with open('model.pkl', 'rb') as file_in:
   model_loaded = cloudpickle.load(file_in)
 
-frase = input('Digite uma frase: ')
+app = Flask(__name__)
 
-if model_loaded.predict([frase])[0] == 0:
-    print('Comentário negativo')
-else:
-    print('Comentário positivo')
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+  comentario = request.form['comentario']
+  sentimento = model_loaded.predict([comentario])[0]
+  print(sentimento)
+  return render_template('resposta.html', resposta=sentimento)
+
+# @app.route("/<frase>")
+# def predict(frase):
+
+#   if model_loaded.predict([frase])[0] == 0:
+#     return 'Comentário negativo'
+#   else:
+#     return 'Comentário positivo'
+
+app.run(debug=True)
